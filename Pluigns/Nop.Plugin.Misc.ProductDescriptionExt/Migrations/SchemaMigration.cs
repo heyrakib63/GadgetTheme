2cf8ca7ab9620e485373d17b400ecd0e1cb337d5
Md. Rakib Hossain <md.rakib@brainstation-23.com>
2025-05-12 02:37:02 +0600

    Created the Extra Description Plugin.

---
+++ Pluigns/Nop.Plugin.Misc.ProductDescriptionExt/Migrations/SchemaMigration.cs
@@ -0,0 +1,14 @@
+﻿using FluentMigrator;
+using Nop.Data.Extensions;
+using Nop.Data.Migrations;
+using Nop.Plugin.Misc.ProductDescriptionExt.Domain;
+
+namespace Nop.Plugin.Misc.ProductDescriptionExt.Migrations;
+[NopSchemaMigration("2025/05/22 07:35:50:1687541", "Misc.ProductDescriptionExt second schema", MigrationProcessType.Installation)]
+public class SchemaMigration : AutoReversingMigration
+{
+    public override void Up()
+    {
+        Create.TableFor<ProductDescription>();
+    }
+}
