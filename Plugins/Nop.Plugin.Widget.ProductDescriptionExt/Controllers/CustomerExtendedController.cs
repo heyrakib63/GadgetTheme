4e3bb677dad50c75560adcbbbb28002a5bcda179
Md. Rakib Hossain <md.rakib@brainstation-23.com>
2025-05-19 20:08:16 +0600

    Integrated the Adress and Customer Info by overriding the controller.

---
+++ Plugins/Nop.Plugin.Widget.ProductDescriptionExt/Controllers/CustomerExtendedController.cs
@@ -0,0 +1,382 @@
+﻿using System.Text.Encodings.Web;
+using Nop.Core;
+using Nop.Core.Domain;
+using Nop.Core.Domain.Common;
+using Nop.Core.Domain.Customers;
+using Nop.Core.Domain.Forums;
+using Nop.Core.Domain.Gdpr;
+using Nop.Core.Domain.Localization;
+using Nop.Core.Domain.Media;
+using Nop.Core.Domain.Security;
+using Nop.Core.Domain.Tax;
+using Nop.Core.Events;
+using Nop.Services.Attributes;
+using Nop.Services.Authentication.External;
+using Nop.Services.Authentication.MultiFactor;
+using Nop.Services.Catalog;
+using Nop.Services.Common;
+using Nop.Services.Customers;
+using Nop.Services.Directory;
+using Nop.Services.ExportImport;
+using Nop.Services.Gdpr;
+using Nop.Services.Helpers;
+using Nop.Services.Localization;
+using Nop.Services.Logging;
+using Nop.Services.Media;
+using Nop.Services.Messages;
+using Nop.Services.Orders;
+using Nop.Services.Security;
+using Nop.Services.Tax;
+using Nop.Web.Controllers;
+using Nop.Web.Factories;
+using Nop.Services.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Nop.Core.Domain.Messages;
+using Nop.Web.Models.Customer;
+
+namespace Nop.Plugin.Widget.ProductDescriptionExt.Controllers;
+
+public partial class CustomerExtendedController : CustomerController
+{
+    public CustomerExtendedController(
+        AddressSettings addressSettings,
+        CaptchaSettings captchaSettings,
+        CustomerSettings customerSettings,
+        DateTimeSettings dateTimeSettings,
+        ForumSettings forumSettings,
+        GdprSettings gdprSettings,
+        HtmlEncoder htmlEncoder,
+        IAddressModelFactory addressModelFactory,
+        IAddressService addressService,
+        IAttributeParser<AddressAttribute, AddressAttributeValue> addressAttributeParser,
+        IAttributeParser<CustomerAttribute, CustomerAttributeValue> customerAttributeParser,
+        IAttributeService<CustomerAttribute, CustomerAttributeValue> customerAttributeService,
+        IAuthenticationService authenticationService,
+        ICountryService countryService,
+        ICurrencyService currencyService,
+        ICustomerActivityService customerActivityService,
+        ICustomerModelFactory customerModelFactory,
+        ICustomerRegistrationService customerRegistrationService,
+        ICustomerService customerService,
+        IDownloadService downloadService,
+        IEventPublisher eventPublisher,
+        IExportManager exportManager,
+        IExternalAuthenticationService externalAuthenticationService,
+        IGdprService gdprService,
+        IGenericAttributeService genericAttributeService,
+        IGiftCardService giftCardService,
+        ILocalizationService localizationService,
+        ILogger logger,
+        IMultiFactorAuthenticationPluginManager multiFactorAuthenticationPluginManager,
+        INewsLetterSubscriptionService newsLetterSubscriptionService,
+        INotificationService notificationService,
+        IOrderService orderService,
+        IPermissionService permissionService,
+        IPictureService pictureService,
+        IPriceFormatter priceFormatter,
+        IProductService productService,
+        IStateProvinceService stateProvinceService,
+        IStoreContext storeContext,
+        ITaxService taxService,
+        IWorkContext workContext,
+        IWorkflowMessageService workflowMessageService,
+        LocalizationSettings localizationSettings,
+        MediaSettings mediaSettings,
+        MultiFactorAuthenticationSettings multiFactorAuthenticationSettings,
+        StoreInformationSettings storeInformationSettings,
+        TaxSettings taxSettings) : base(
+            addressSettings,
+            captchaSettings,
+            customerSettings,
+            dateTimeSettings,
+            forumSettings,
+            gdprSettings,
+            htmlEncoder,
+            addressModelFactory,
+            addressService,
+            addressAttributeParser,
+            customerAttributeParser,
+            customerAttributeService,
+            authenticationService,
+            countryService,
+            currencyService,
+            customerActivityService,
+            customerModelFactory,
+            customerRegistrationService,
+            customerService,
+            downloadService,
+            eventPublisher,
+            exportManager,
+            externalAuthenticationService,
+            gdprService,
+            genericAttributeService,
+            giftCardService,
+            localizationService,
+            logger,
+            multiFactorAuthenticationPluginManager,
+            newsLetterSubscriptionService,
+            notificationService,
+            orderService,
+            permissionService,
+            pictureService,
+            priceFormatter,
+            productService,
+            stateProvinceService,
+            storeContext,
+            taxService,
+            workContext,
+            workflowMessageService,
+            localizationSettings,
+            mediaSettings,
+            multiFactorAuthenticationSettings,
+            storeInformationSettings,
+            taxSettings)
+    {
+
+    }
+
+    [HttpPost]
+    public override async Task<IActionResult> Info(CustomerInfoModel model, IFormCollection form)
+    {
+        var customer = await _workContext.GetCurrentCustomerAsync();
+        if (!await _customerService.IsRegisteredAsync(customer))
+            return Challenge();
+
+        var oldCustomerModel = new CustomerInfoModel();
+
+        //get customer info model before changes for gdpr log
+        if (_gdprSettings.GdprEnabled & _gdprSettings.LogUserProfileChanges)
+            oldCustomerModel = await _customerModelFactory.PrepareCustomerInfoModelAsync(oldCustomerModel, customer, false);
+
+        //custom customer attributes
+        var customerAttributesXml = await ParseCustomCustomerAttributesAsync(form);
+        var customerAttributeWarnings = await _customerAttributeParser.GetAttributeWarningsAsync(customerAttributesXml);
+        foreach (var error in customerAttributeWarnings)
+        {
+            ModelState.AddModelError("", error);
+        }
+
+        //GDPR
+        if (_gdprSettings.GdprEnabled)
+        {
+            var consents = (await _gdprService
+                .GetAllConsentsAsync()).Where(consent => consent.DisplayOnCustomerInfoPage && consent.IsRequired).ToList();
+
+            ValidateRequiredConsents(consents, form);
+        }
+
+        try
+        {
+            if (ModelState.IsValid)
+            {
+                //username 
+                if (_customerSettings.UsernamesEnabled && _customerSettings.AllowUsersToChangeUsernames)
+                {
+                    var userName = model.Username;
+                    if (!customer.Username.Equals(userName, StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        //change username
+                        await _customerRegistrationService.SetUsernameAsync(customer, userName);
+
+                        //re-authenticate
+                        //do not authenticate users in impersonation mode
+                        if (_workContext.OriginalCustomerIfImpersonated == null)
+                            await _authenticationService.SignInAsync(customer, true);
+                    }
+                }
+                //email
+                var email = model.Email;
+                if (!customer.Email.Equals(email, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    //change email
+                    var requireValidation = _customerSettings.UserRegistrationType == UserRegistrationType.EmailValidation;
+                    await _customerRegistrationService.SetEmailAsync(customer, email, requireValidation);
+
+                    //do not authenticate users in impersonation mode
+                    if (_workContext.OriginalCustomerIfImpersonated == null)
+                    {
+                        //re-authenticate (if usernames are disabled)
+                        if (!_customerSettings.UsernamesEnabled && !requireValidation)
+                            await _authenticationService.SignInAsync(customer, true);
+                    }
+                }
+
+                //properties
+                if (_dateTimeSettings.AllowCustomersToSetTimeZone)
+                    customer.TimeZoneId = model.TimeZoneId;
+                //VAT number
+                if (_taxSettings.EuVatEnabled)
+                {
+                    var prevVatNumber = customer.VatNumber;
+                    customer.VatNumber = model.VatNumber;
+
+                    if (prevVatNumber != model.VatNumber)
+                    {
+                        var (vatNumberStatus, _, vatAddress) = await _taxService.GetVatNumberStatusAsync(model.VatNumber);
+                        customer.VatNumberStatusId = (int)vatNumberStatus;
+
+                        //send VAT number admin notification
+                        if (!string.IsNullOrEmpty(model.VatNumber) && _taxSettings.EuVatEmailAdminWhenNewVatSubmitted)
+                            await _workflowMessageService.SendNewVatSubmittedStoreOwnerNotificationAsync(customer,
+                                model.VatNumber, vatAddress, _localizationSettings.DefaultAdminLanguageId);
+                    }
+                }
+
+                //form fields
+                if (_customerSettings.GenderEnabled)
+                    customer.Gender = model.Gender;
+                if (_customerSettings.FirstNameEnabled)
+                    customer.FirstName = model.FirstName;
+                if (_customerSettings.LastNameEnabled)
+                    customer.LastName = model.LastName;
+                if (_customerSettings.DateOfBirthEnabled)
+                    customer.DateOfBirth = model.ParseDateOfBirth();
+                if (_customerSettings.CompanyEnabled)
+                    customer.Company = model.Company;
+                if (_customerSettings.StreetAddressEnabled)
+                    customer.StreetAddress = model.StreetAddress;
+                if (_customerSettings.StreetAddress2Enabled)
+                    customer.StreetAddress2 = model.StreetAddress2;
+                if (_customerSettings.ZipPostalCodeEnabled)
+                    customer.ZipPostalCode = model.ZipPostalCode;
+                if (_customerSettings.CityEnabled)
+                    customer.City = model.City;
+                if (_customerSettings.CountyEnabled)
+                    customer.County = model.County;
+                if (_customerSettings.CountryEnabled)
+                    customer.CountryId = model.CountryId;
+                if (_customerSettings.CountryEnabled && _customerSettings.StateProvinceEnabled)
+                    customer.StateProvinceId = model.StateProvinceId;
+                if (_customerSettings.PhoneEnabled)
+                    customer.Phone = model.Phone;
+                if (_customerSettings.FaxEnabled)
+                    customer.Fax = model.Fax;
+
+                customer.CustomCustomerAttributesXML = customerAttributesXml;
+                await _customerService.UpdateCustomerAsync(customer);
+
+                //After updating the customer table, we also update the address table
+
+                //find address (ensure that it belongs to the current customer)
+                var addresses = await _customerService.GetAddressesByCustomerIdAsync(customer.Id);
+                if (addresses == null)
+                    //address is not found
+                    return RedirectToRoute("CustomerInfo");
+
+                var address = addresses.First();
+                address.FirstName = model.FirstName;
+                address.LastName = model.LastName;
+
+                if (ModelState.IsValid)
+                {
+                    await _addressService.UpdateAddressAsync(address);
+                }
+
+                //newsletter
+                if (_customerSettings.NewsletterEnabled)
+                {
+                    //save newsletter value
+                    var store = await _storeContext.GetCurrentStoreAsync();
+                    var newsletter = await _newsLetterSubscriptionService.GetNewsLetterSubscriptionByEmailAndStoreIdAsync(customer.Email, store.Id);
+                    if (newsletter != null)
+                    {
+                        if (model.Newsletter)
+                        {
+                            newsletter.Active = true;
+                            await _newsLetterSubscriptionService.UpdateNewsLetterSubscriptionAsync(newsletter);
+                        }
+                        else
+                        {
+                            await _newsLetterSubscriptionService.DeleteNewsLetterSubscriptionAsync(newsletter);
+                        }
+                    }
+                    else
+                    {
+                        if (model.Newsletter)
+                        {
+                            await _newsLetterSubscriptionService.InsertNewsLetterSubscriptionAsync(new NewsLetterSubscription
+                            {
+                                NewsLetterSubscriptionGuid = Guid.NewGuid(),
+                                Email = customer.Email,
+                                Active = true,
+                                StoreId = store.Id,
+                                LanguageId = customer.LanguageId ?? store.DefaultLanguageId,
+                                CreatedOnUtc = DateTime.UtcNow
+                            });
+                        }
+                    }
+                }
+
+                if (_forumSettings.ForumsEnabled && _forumSettings.SignaturesEnabled)
+                    await _genericAttributeService.SaveAttributeAsync(customer, NopCustomerDefaults.SignatureAttribute, model.Signature);
+
+                //GDPR
+                if (_gdprSettings.GdprEnabled)
+                    await LogGdprAsync(customer, oldCustomerModel, model, form);
+
+                _notificationService.SuccessNotification(await _localizationService.GetResourceAsync("Account.CustomerInfo.Updated"));
+
+                return RedirectToRoute("CustomerInfo");
+            }
+        }
+        catch (Exception exc)
+        {
+            ModelState.AddModelError("", exc.Message);
+        }
+
+        //If we got this far, something failed, redisplay form
+        model = await _customerModelFactory.PrepareCustomerInfoModelAsync(model, customer, true, customerAttributesXml);
+
+        return View(model);
+    }
+
+    [HttpPost]
+    public override async Task<IActionResult> AddressEdit(CustomerAddressEditModel model, IFormCollection form)
+    {
+        var customer = await _workContext.GetCurrentCustomerAsync();
+        if (!await _customerService.IsRegisteredAsync(customer))
+            return Challenge();
+
+        //find address (ensure that it belongs to the current customer)
+        var address = await _customerService.GetCustomerAddressAsync(customer.Id, model.Address.Id);
+        if (address == null)
+            //address is not found
+            return RedirectToRoute("CustomerAddresses");
+
+        //custom address attributes
+        var customAttributes = await _addressAttributeParser.ParseCustomAttributesAsync(form, NopCommonDefaults.AddressAttributeControlName);
+        var customAttributeWarnings = await _addressAttributeParser.GetAttributeWarningsAsync(customAttributes);
+        foreach (var error in customAttributeWarnings)
+        {
+            ModelState.AddModelError("", error);
+        }
+
+        if (ModelState.IsValid)
+        {
+            //First update the address table
+            address = model.Address.ToEntity(address);
+            address.CustomAttributes = customAttributes;
+            await _addressService.UpdateAddressAsync(address);
+
+            //Then update the customer table
+            customer.FirstName = model.Address.FirstName;
+            customer.LastName = model.Address.LastName;
+            await _customerService.UpdateCustomerAsync(customer);
+
+            _notificationService.SuccessNotification(await _localizationService.GetResourceAsync("Account.CustomerAddresses.Updated"));
+
+            return RedirectToRoute("CustomerAddresses");
+        }
+
+        //If we got this far, something failed, redisplay form
+        await _addressModelFactory.PrepareAddressModelAsync(model.Address,
+            address: address,
+            excludeProperties: true,
+            addressSettings: _addressSettings,
+            loadCountries: async () => await _countryService.GetAllCountriesAsync((await _workContext.GetWorkingLanguageAsync()).Id),
+            overrideAttributesXml: customAttributes);
+
+        return View(model);
+    }
+}
--- Plugins/Nop.Plugin.Widget.ProductDescriptionExt/Infrastructure/NopStartup.cs
+++ Plugins/Nop.Plugin.Widget.ProductDescriptionExt/Infrastructure/NopStartup.cs
@@ -3,7 +3,9 @@ using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Nop.Core.Infrastructure;
+using Nop.Plugin.Widget.ProductDescriptionExt.Controllers;
 using Nop.Plugin.Widget.ProductDescriptionExt.Services;
+using Nop.Web.Controllers;
 
 namespace Nop.Plugin.Widget.ProductDescriptionExt.Infrastructure;
 public class NopStartup : INopStartup
@@ -15,9 +17,10 @@ public class NopStartup : INopStartup
             options.ViewLocationExpanders.Add(new ViewLocationExpander());
         });
         services.AddScoped<IProductDescriptionService, ProductDescriptionService>();
+        services.AddScoped<CustomerController, CustomerExtendedController>();
     }
     public void Configure(IApplicationBuilder application)
     {
     }
-    public int Order => 1000;
+    public int Order => 3050;
 }
